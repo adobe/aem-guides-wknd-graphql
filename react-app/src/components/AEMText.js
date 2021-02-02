f46c88caaf47fbab813fd07eaceda800111bb524
sharanyavinod <sharayavinod90@gmail.com>
2021-02-02 19:36:52 +0100

    Components added, updated resourcetypes

--- react-app/src/components/AEMText.js
+++ react-app/src/components/AEMText.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
 import { withMappable, MapTo } from '@adobe/aem-react-editable-components';
-import DOMPurify from 'dompurify';
+import { TextV2, TextV2IsEmptyFn } from "@adobe/aem-core-components-react-base";
 
 /**
  * Default Edit configuration for the Text component that interact with the Core Text component and sub-types
@@ -9,37 +8,10 @@ import DOMPurify from 'dompurify';
  */
 const TextEditConfig = {
   emptyLabel: 'Text',
-
-  isEmpty: function(props) {
-    return !props || !props.text || props.text.trim().length < 1;
-  },
-  resourceType: "wknd-spa-react/components/text"
+  isEmpty: TextV2IsEmptyFn,
+  resourceType: 'wknd-spa/components/text'
 };
 
-/**
- * Text React component
- */
-class Text extends Component {
-  get richTextContent() {
-    return (
-      <div
-        data-rte-editelement
-        dangerouslySetInnerHTML={{
-          __html: DOMPurify.sanitize(this.props.text)
-        }}
-      />
-    );
-  }
+export const AEMText = withMappable(TextV2, TextEditConfig);
 
-  get textContent() {
-    return <div>{this.props.text}</div>;
-  }
-
-  render() {
-    return this.props.richText ? this.richTextContent : this.textContent;
-  }
-}
-
-export const AEMText = withMappable(Text, TextEditConfig);
-
-MapTo('wknd-spa-react/components/text')(Text, TextEditConfig);
+MapTo('wknd-spa/components/text')(TextV2, TextEditConfig);
