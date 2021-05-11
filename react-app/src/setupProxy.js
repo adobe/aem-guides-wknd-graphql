37923af58b2d9b67b330f19d3999aa4ff6640bfc
Adrian Kozma <kozma@adobe.com>
2021-05-11 22:20:32 +0200

    Updates authentication methods and defaults.

--- react-app/src/setupProxy.js
+++ react-app/src/setupProxy.js
@@ -6,24 +6,22 @@ NOTICE: Adobe permits you to use, modify, and distribute this file in
 accordance with the terms of the Adobe license agreement accompanying
 it.
 */
-
-const { SERVICE_TOKEN, DEV_TOKEN, BASIC, NONE } = require('./proxy/authMethods');
+const { SERVICE_TOKEN, DEV_TOKEN, BASIC } = require('./proxy/authMethods');
 
 const proxy = (() => {
     switch (process.env.REACT_APP_AUTH_METHOD) {
-        case SERVICE_TOKEN:
-            // Use Service token exchange for Cloud Env PROD
-            return require('./proxy/setupProxy.auth.service-token');
-        case DEV_TOKEN:
-            // Use Dev token for local development with Cloud Env
-            return require('./proxy/setupProxy.auth.dev-token');
-        case NONE:
-            // Auth not needed for local development with Local Publisher Env
-            return require('./proxy/setupProxy.auth.none');
-        case BASIC:
-        default:
-            // Use user/pass for local development with Local Author Env
-            return require('./proxy/setupProxy.auth.basic');
+      case SERVICE_TOKEN:
+        // Use Service token exchange for Cloud Env PROD
+        return require('./proxy/setupProxy.auth.service-token');
+      case DEV_TOKEN:
+        // Use Dev token for local development with Cloud Env
+        return require('./proxy/setupProxy.auth.dev-token');
+      case BASIC:
+        // Use user/pass for local development with Local Author Env
+        return require('./proxy/setupProxy.auth.basic');
+      default:
+        // Auth not needed for local development with Local Publisher Env
+        return require('./proxy/setupProxy.auth.none');
     }
 })();
 
