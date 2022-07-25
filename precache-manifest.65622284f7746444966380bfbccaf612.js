53def2060c258f322e99883ebdd70830bc4bf1d9
Sachin Mali <smali@adobe.com>
2022-07-25 18:07:31 -0400

    Updates

---
+++ precache-manifest.65622284f7746444966380bfbccaf612.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "806f0168be859175a3322f575b1e3e7e",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "05192c3e38075a5d11a3",
+    "url": "/react-gh-pages/static/css/main.c08dfc50.chunk.css"
+  },
+  {
+    "revision": "6b514714b80cb55f29cb",
+    "url": "/react-gh-pages/static/js/2.940311f3.chunk.js"
+  },
+  {
+    "revision": "a7dbb2f7c1982df250f29be04fea8f2a",
+    "url": "/react-gh-pages/static/js/2.940311f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05192c3e38075a5d11a3",
+    "url": "/react-gh-pages/static/js/main.5c7759bf.chunk.js"
+  },
+  {
+    "revision": "4e5b46e606a3fd10ebac",
+    "url": "/react-gh-pages/static/js/runtime-main.e4a625fd.js"
+  },
+  {
+    "revision": "200bc7ed9cfe0f3bde2835852b50271e",
+    "url": "/react-gh-pages/static/media/icon-loading.200bc7ed.svg"
+  },
+  {
+    "revision": "180f5e5f65a031a0bed8fca8bc79fdfb",
+    "url": "/react-gh-pages/static/media/wknd-icon.180f5e5f.svg"
+  }
+]);
