a73621f0895d36a14ad50705263aa0604cd02fa3
Hannes Hertach <hanneshertach@gmail.com>
2022-09-12 17:54:05 +0200

    add CRA rewired

---
+++ react-app/config-overrides.js
@@ -0,0 +1,22 @@
+module.exports = function override(config, env) {
+  config.optimization.splitChunks = {
+    cacheGroups: {
+      default: false,
+    },
+  };
+  config.optimization.runtimeChunk = false;
+
+  config.module.rules = [
+    {
+      test: /node_modules\/@adobe\/aem-react-editable-components/,
+      use: {
+        loader: 'babel-loader',
+        // if you include your babel config here,
+        // you don’t need the `babel.config.json` file
+        options: { presets: ['@babel/preset-env'] }
+      }
+    },
+    ...config.module.rules
+  ]
+  return config;
+}
