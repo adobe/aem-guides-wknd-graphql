1d852c3dddb7cf7094b1b5476e5cb97292be53e1
Sachin Mali <sachinpree@gmail.com>
2022-10-14 13:26:40 -0400

    Removed implementation and added TODO comments

--- basic-tutorial/src/api/usePersistedQueries.js
+++ basic-tutorial/src/api/usePersistedQueries.js
@@ -33,27 +33,10 @@ import { useEffect, useState } from "react";
  * @returns the GraphQL data or an error message 
  */
 async function fetchPersistedQuery(persistedQueryName, queryParameters) {
-  let data;
-  let err;
-
-  try {
-    // AEM GraphQL queries are asynchronous, either await their return or use Promise-based .then(..) { ... } syntax
-    const response = await aemHeadlessClient.runPersistedQuery(
-      persistedQueryName,
-      queryParameters
-    );
-    // The GraphQL data is stored on the response's data field
-    data = response?.data;
-  } catch (e) {
-    // An error occurred, return the error messages
-    err = e
-      .toJSON()
-      ?.map((error) => error.message)
-      ?.join(", ");
-    console.error(e.toJSON());
-  }
-
-  return { data, err };
+  //*********************************
+  // TODO :: Implement this by following the steps from AEM Headless Tutorial ==> Connect to AEM GraphQL endpoints
+  // https://experienceleague.adobe.com/docs/experience-manager-learn/getting-started-with-aem-headless/graphql/multi-step/graphql-and-react-app.html#connect-to-aem-graphql-endpoints
+  //*********************************
 }
 
 /**
@@ -62,25 +45,11 @@ async function fetchPersistedQuery(persistedQueryName, queryParameters) {
  * @returns an array of Team JSON objects, and array of errors
  */
 export function useAllTeams() {
-  const [teams, setTeams] = useState(null);
-  const [errors, setErrors] = useState(null);
 
-  // Use React useEffect to manage state changes
-  useEffect(() => {
-    async function fetchData() {
-      // Call the AEM GraphQL persisted query named "my-project/all-teams"
-      const { data, err } = await fetchPersistedQuery("my-project/all-teams");
-      // Sets the teams variable to the list of team JSON objects
-      setTeams(data?.teamList?.items);
-      // Set any errors
-      setErrors(err);
-    }
-    // Call the internal fetchData() as per React best practices
-    fetchData();
-  }, []);
-
-  // Returns the teams and errors
-  return { teams, errors };
+  //*********************************
+  // TODO :: Implement this by following the steps from AEM Headless Tutorial ==> Implement Teams functionality
+  // https://experienceleague.adobe.com/docs/experience-manager-learn/getting-started-with-aem-headless/graphql/multi-step/graphql-and-react-app.html#implement-teams-functionality
+  //*********************************
 }
 
 /**
@@ -90,33 +59,10 @@ export function useAllTeams() {
  * @returns a JSON object representing the person
  */
 export function usePersonByName(fullName) {
-  const [person, setPerson] = useState(null);
-  const [errors, setErrors] = useState(null);
 
-  useEffect(() => {
-    async function fetchData() {
-      // The key is the variable name as defined in the persisted query, and may not match the model's field name
-      const queryParameters = { name: fullName };
-      
-      // Invoke the persisted query, and pass in the queryParameters object as the 2nd parameter
-      const { data, err } = await fetchPersistedQuery(
-        "my-project/person-by-name",
-        queryParameters
-      );
+  //*********************************
+  // TODO :: Implement this by following the steps from AEM Headless Tutorial ==> Implement Person functionality
+  // https://experienceleague.adobe.com/docs/experience-manager-learn/getting-started-with-aem-headless/graphql/multi-step/graphql-and-react-app.html#implement-person-functionality
+  //*********************************
 
-      if (err) {
-        // Capture errors from the HTTP request
-        setErrors(err);
-      } else if (data?.personList?.items?.length === 1) {
-        // Set the person data after data validation
-        setPerson(data.personList.items[0]);
-      } else {
-        // Set an error if no person could be found
-        setErrors(`Cannot find person with name: ${fullName}`);
-      }
-    }
-    fetchData();
-  }, [fullName]);
-
-  return { person, errors };
 }
