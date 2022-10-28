177265731ea52e2853c9ccf880e4e86018efa69f
Hannes Hertach <hanneshertach@gmail.com>
2022-10-28 12:42:30 +0200

    add comment about routed link

--- react-app/src/components/editable/core/RoutedLink.js
+++ react-app/src/components/editable/core/RoutedLink.js
@@ -1,6 +1,9 @@
 import {Link as RouterLink} from "react-router-dom";
 import React from "react";
 
+// This component is used internally in the Image and Text components
+// Core Components Dialogs use path pickers that encourage an author to select a CQ Page resource. There is no
+// autocomplete to SPA routes in the pickers. Please be aware of this when using RoutedLink.
 export const RoutedLink = (props) => {
   const {to, isRouted, ...otherProps} = props;
 
