761f8fe08b024973f1ce665a95a4fde5fd3993bc
david g <davidjgonzalez@users.noreply.github.com>
2023-03-17 13:37:12 -0400

    Preview tutorial (#84)

---
+++ preview-tutorial/src/App.js
@@ -0,0 +1,42 @@
+/*
+Copyright 2022 Adobe
+All Rights Reserved.
+
+NOTICE: Adobe permits you to use, modify, and distribute this file in
+accordance with the terms of the Adobe license agreement accompanying
+it.
+*/
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ScrollToTop from "./utils/scrollToTop";
+import logo from "./images/wknd-logo-dk.svg";
+import Home from "./components/Home";
+import AdventureDetail from "./components/AdventureDetail";
+import { Link } from "react-router-dom";
+import "./App.scss";
+
+function App() {
+  return (
+    <Router>
+      <ScrollToTop />
+      <div className="App">
+        <header>
+        <Link to={"/"}>
+          <img src={logo} className="logo" alt="WKND Logo"/>
+        </Link>        
+        <hr />
+        </header>
+        <Switch>
+          <Route path='/adventure/*'>
+            <AdventureDetail />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
